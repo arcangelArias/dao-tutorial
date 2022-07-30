ee6bfc439b0f8d7f4c7b1782c0b13019b2f76b75
angelArias <angel@angelarias.es>
2022-07-30 20:44:36 +0200

    subo dao-tutorial

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,6 @@
+
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xc25bd7726230d121fa19820c5A30cFf260507946";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
